514fbab6e2f0a8f7d9acef0a6158943ace313b5e
Breno Bernardes <breno.pbernardes@gmail.com>
2022-06-13 20:39:33 -0300

    email validation updated

--- src/pages/form/form.js
+++ src/pages/form/form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Form, Button } from "react-bootstrap";
 import api from "../../api/api";
 import "./form.scss";
@@ -10,94 +10,85 @@ export default function Forms () {
     const [zipCode, setZipCode] = useState([]);
 
     useEffect(() => {
-        api
-        .get(`/${zipCode}`)
-        .then((response) => setZipCode(response.data))
-        .catch((err) => {
-            console.log("ops! ocorreu um erro" + err);
-        });
+        if (zipCode.length === 8) {
+            api
+            .get(`/${zipCode}`)
+            .then((response) => setZipCode(response.data))
+            .catch((err) => {
+                console.log("ops! ocorreu um erro" + err);
+            });
+        } else if (zipCode.length <= 7) {
+            console.log("Deve ser maior que 8")
+        }
     });
 
-// Validação do email
+    // Validação do email
 
     const [email, setEmail] = useState();
+    const emailReturn = useRef("");
 
     function validateEmail (e) {
         setEmail(e.target.value);
         const emailIsValid = /\S+@\S+\.\S+/;
 
         if (emailIsValid.test(email)) {
-            return console.log('Valid Email')
+            emailReturn.current = "Email válido";
+            console.log("email valido")
         } else {
-            return console.log('Enter Valid Email')
+            emailReturn.current = "Email inválido";
+            console.log("email invalido")
         }
     }
 
     return (
         <div className="mainForm">
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="infoGroup" controlId="formBasicInfo">
+                    <h2>Dados Gerais</h2>
+
                     <Form.Label>Nome Completo</Form.Label>
                     <Form.Control type="text" placeholder="Nome Completo" />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Data de Nascimento</Form.Label>
                     <Form.Control type="date" placeholder="Data de Nascimento" />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>CPF</Form.Label>
                     <Form.Control type="number" placeholder="CPF" />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Telefone</Form.Label>
                     <Form.Control type="number" placeholder="(31)12345-6789" />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type="email" placeholder="Email" onChange={validateEmail} />
-                    <p>{}</p>
+                    <p>{emailReturn.current}</p>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="addressGroup" controlId="formBasicAddress">
+                    <h2>Endereço</h2>
+
                     <Form.Label>CEP</Form.Label>
                     <Form.Control type="number" placeholder="CEP" onChange={e => setZipCode(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                
                     <Form.Label>Logradouro</Form.Label>
                     <Form.Control type="text" placeholder="Rua: " value={zipCode.logradouro} />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Número</Form.Label>
                     <Form.Control type="number" placeholder="000" />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Complemento</Form.Label>
                     <Form.Control type="text" placeholder="Complemento" />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Bairro</Form.Label>
                     <Form.Control type="text" placeholder="Bairro" value={zipCode.bairro} />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Cidade</Form.Label>
                     <Form.Control type="text" placeholder="Cidade" value={zipCode.cidade} />
-                </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>UF</Form.Label>
                     <Form.Control type="text" placeholder="UF" value={zipCode.estado} />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="descriptionGroup" controlId="formBasicPassword">
                     <Form.Label>Descrição</Form.Label>
                     <Form.Control type="text" placeholder="Bio" />
                     <Form.Text></Form.Text>
